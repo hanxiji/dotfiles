8ef1521faf3d0e9bc18841f4bb8090cec725d847
xxj <hanxiji@outlook.com>
2026-07-29 23:46:52 +0800

    nvim配置更新
    - 新增markdown-preview插件
    - 插件版本更新

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -12,5 +12,25 @@ return {
         ---@module 'render-markdown'
         ---@type render.md.UserConfig
         opts = {},
+    },
+    {
+        "selimacerbas/markdown-preview.nvim",
+        dependencies = { "selimacerbas/live-server.nvim" },
+        config = function()
+            require("markdown_preview").setup({
+                -- all optional; sane defaults shown
+                instance_mode = "takeover",  -- "takeover" (one tab) or "multi" (tab per instance)
+                port = 0,                    -- 0 = auto (8421 for takeover, OS-assigned for multi)
+                default_theme = "dark",      -- "dark" or "light"; initial preview theme
+                debounce_ms = 300,
+                host = "0.0.0.0",
+                open_browser = false,
+                hooks = {
+                    on_start = function(url)
+                        vim.notify("Markdown Preview: " .. url, vim.log.levels.INFO)
+                    end,
+                },
+            })
+        end,
     }
 }
